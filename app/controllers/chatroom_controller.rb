b859fe3c93e96e31953014f94cdda2b7b301c244
shady <shadyeldawy96@gmail.com>
2020-02-27 15:54:26 +0200

    Add message partial and complete links to login and chatroom

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,7 @@
 class ChatroomController < ApplicationController
 
 	def index
+		@messages = Message.all #inistance to grab all messages to use in the index view page
 
 	end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+User.create(username: "ali" ,password: "pass")
+Message.create(body: "hello world!" ,user_id: "1")
+Message.create(body: "hello world!" ,user: User.last)
