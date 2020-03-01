522b6c7291dc632eb793784f108d8cae107119e1
shady <shadyeldawy96@gmail.com>
2020-03-01 12:48:39 +0200

    add form_for login page and sessions login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+	helper_method :current_user, :logged_in?
+
+
+	def current_user
+		@current_user ||= User.find(session[:user_id]) if session[:user_id] #if there is a user backed by browser, look for this user id in database and return it.
+		# ||= is for return current user or find it ||=
+
+	end
+
+	def logged_in? #to see that current_user method is true or not
+		!!current_user #return true if i have a current user 
+	end
+
+
+	def require_user
+		if !logged_in?
+			flash[:error] = "you must be logged in to perform that action"
+			redirect_to login_path
+		end
+	end
+
+
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,24 @@ class SessionsController < ApplicationController
 	def new
 		
 	end 
+
+	def create
+		user = User.find_by(username: params[:session][:username]) #compare user input (params[][]) by users in database
+		if user && user.authenticate(params[:session][:password])
+			session[:user_id] = user.id #set session id user to user id that was found above
+			flash[:success] = "You have successfully logged in"
+			redirect_to root_path 
+
+		else
+			flash.now[:error] = "There was something wrong with your login information" #flash now shows message in same page not in the second page
+			render 'new'
+		end
+
+	end 
+
+	def destroy
+		session[:user_id] = nil
+		flash[:success] = "You have successfully Logged out"
+		redirect_to login_path
+	end 
 end
